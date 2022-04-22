81ff340067a2ddf3043459d7e59a20000705666d
vinh922004 <104201641+vinh922004@users.noreply.github.com>
2022-04-22 17:50:50 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "Tiêu đề web",
+    introTitle: 'Tên người yêu bạn',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: '^^HiHi^^',
+    title: 'Anh có phải là người xấu xí nhất thiên hạ ko 🥰',
+    desc: 'Phải chăng là như vậy        ',
+    btnYes: 'Xấu lắm nha <33',
+    btnNo: 'Không nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn nói mình xấu <3',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Xấu kệ tui liên quan gì mấy người <33333333',
+    mess: 'Mình biết mà 🥰. Mình xấu lắm luôn 😘😘',
+    messDesc: 'Tối nay 7h, mình qua đập tú chơi nha.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100070834524073'
+}
